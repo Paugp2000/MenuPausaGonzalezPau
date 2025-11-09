9196b8a57ab58ad165112b102faf3891e0aa9d11
Paugp2000 <a24paugonpal@inspedralbes.cat>
2025-11-09 18:15:25 +0100

    Build incluida

--- MenuPausaGonzalezPau/Assets/DropdownScreen.cs
+++ MenuPausaGonzalezPau/Assets/DropdownScreen.cs
@@ -1,11 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
-using UnityEditor.UI;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 public class DropdownScreen : MonoBehaviour
 {
